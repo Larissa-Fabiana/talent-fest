51e6fe336db4550fc5c732566366c88e525481ae
Deise Trianon <deisetrianon@gmail.com>
2018-11-08 11:39:38 -0200

    primeira versão de firebase auth e database

---
+++ index.js
@@ -0,0 +1,70 @@
+var database = firebase.database();
+
+$(document).ready(function() {
+  fadePage();
+  $(".sign-up-button").click(signUpClick);
+  $(".sign-in-button").click(signInClick);
+});
+
+function fadePage() {
+  $('#fade-initial-page').delay('2000').fadeOut('slow');
+  $('#main-page').delay('2000').fadeIn('slow');
+}
+
+function signUpClick(event) {
+  event.preventDefault();
+  
+  var name = $('.sign-up-name').val();
+  var email = $(".sign-up-email").val();
+  var password = $(".sign-up-password").val();
+
+  createUser(name, email, password);
+}
+
+function createUser(name, email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(response) {
+      var userId = response.user.uid;
+      userInfo(userId, name, email, password);
+      redirectToPosts(userId);
+    })
+    .catch(function(error) {
+      handleError(error);
+    });
+}
+
+function signInClick(event) {
+  event.preventDefault();
+
+  var email = $(".sign-in-email").val();
+  var password = $(".sign-in-password").val();
+
+  signInUser(email, password);
+}
+
+function signInUser(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(response) {
+      var userId = response.user.uid;
+      redirectToPosts(userId);
+    })
+    .catch(function(error) {
+      handleError(error);
+    });
+}
+
+function handleError(error) {
+  var errorMessage = error.message;
+  alert(errorMessage);
+}
+
+function redirectToPosts(userId) {
+  window.location = "posts.html?id=" + userId;
+}
+
+function userInfo(userId, name, email) {
+  database.ref('users/' + userId).set({
+    name: name,
+    email: email
+  });
+}
