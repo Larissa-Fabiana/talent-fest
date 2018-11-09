d84a6ae6559b791ecaf124009d33af8a6eff9651
Larissa-Fabiana <ferreira-larissa@outlook.com>
2018-11-09 13:39:59 -0200

    arquivos pre-teste hosting

---
+++ public/firebase-init.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAcZmBL5hRZaPYzoqacpxFZzRJl2oEbiy4",
+  authDomain: "talent-fest-bradesco.firebaseapp.com",
+  databaseURL: "https://talent-fest-bradesco.firebaseio.com",
+  projectId: "talent-fest-bradesco",
+  storageBucket: "",
+  messagingSenderId: "982320024984"
+};
+firebase.initializeApp(config);
